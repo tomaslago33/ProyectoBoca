d1edd3c5aa70aeb0f520308c453999df670324ee
Agustin Eslava <aguseslava11@gmail.com>
2026-05-08 20:42:17 -0300

    Se modifico estilos y js

--- script.js
+++ script.js
@@ -1,20 +1,75 @@
 const track = document.getElementById('track');
-const diapositivas = document.querySelectorAll('.diapositiva');
+let diapositivas = document.querySelectorAll('.diapositiva');
 let indiceActual = 0;
 
-function moverCarrusel() {
-    // Incrementamos el índice
-    indiceActual++;
+let isDragging = false;
+let startPos = 0;
+let currentTranslate = 0;
+let prevTranslate = 0;
+let animationID = 0;
 
-    if (indiceActual >= diapositivas.length) {
-        indiceActual = 0;
-    }
+// 1. EVENTOS DE MOUSE
+// click (Mano abierta -> Mano cerrada)
+track.addEventListener('mousedown', dragStart);
+// sueltar el click
+track.addEventListener('mouseup', dragEnd);
+//  movimiento de mouse
+track.addEventListener('mousemove', dragAction);
+//  el mouse sale del carrusel
+track.addEventListener('mouseleave', dragEnd);
 
-    
-    const desplazamiento = indiceActual * -100;
-    
-    // Aplicamos el movimiento con la propiedad transform
-    track.style.transform = `translateX(${desplazamiento}%)`;
+
+function dragStart(event) {
+    isDragging = true;
+    startPos = event.clientX;
+    track.style.transition = 'none';
 }
 
-setInterval(moverCarrusel, 5000);
+function dragAction(event) {
+    if (!isDragging) return;
+    
+    const currentPosition = event.clientX;
+    const diff = currentPosition - startPos;
+    
+    const desplazamientoManual = (indiceActual * -window.innerWidth) + diff;
+    track.style.transform = `translateX(${desplazamientoManual}px)`;
+}
+
+function dragEnd(event) {
+    if (!isDragging) return;
+    isDragging = false;
+    
+    const endPos = event.clientX;
+    const movedBy = endPos - startPos;
+
+    if (movedBy < -100 && indiceActual < diapositivas.length - 1) {
+        indiceActual++;
+    } else if (movedBy > 100 && indiceActual > 0) {
+        indiceActual--;
+    }
+
+    actualizarPosicion();
+}
+
+// 2. FUNCIÓN PARA ACTUALIZAR EL MOVIMIENTO 
+function actualizarPosicion() {
+    track.style.transition = 'transform 0.8s cubic-bezier(0.45, 0, 0.55, 1)';
+    const desplazamiento = indiceActual * -100;
+    track.style.transform = `translateX(${desplazamiento}vw)`;
+}
+
+
+
+// 3. MOVIMIENTO AUTOMÁTICO 
+  function moverAutomaticamente() {
+    if (!isDragging) { 
+        indiceActual++;
+        if (indiceActual >= diapositivas.length) {
+            indiceActual = 0;
+        }
+        actualizarPosicion();
+    }
+}
+
+// movimiento cada 5 segundos
+setInterval(moverAutomaticamente, 5000);
