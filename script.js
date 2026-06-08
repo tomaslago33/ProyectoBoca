1a92b40a4d4e5978242d4c006c023666a1950bf8
Agustin Eslava <aguseslava11@gmail.com>
2026-06-08 20:39:57 -0300

    commit de boca shop agustin

--- script.js
+++ script.js
@@ -117,3 +117,17 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 });
+
+const links = document.querySelectorAll(".linkcanal");
+links.forEach(link => {
+    link.addEventListener("mouseenter",() => {
+        link.classList.remove("hover-in","hover-out");
+        void link.offsetWidth;
+        link.classList.add("hover-in")
+    });
+link.addEventListener("mouseleave", () => {
+    link.classList.remove("hover-in","hover-out");
+    void link.offsetWidth;
+    link.classList.add("hover-out");   
+    });
+})
